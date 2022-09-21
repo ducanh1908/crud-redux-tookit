0f742352cc3a251e946e6d2851a0828e1cbc464c
DucAnh <ducanh.le1908@gmail.com>
2022-09-21 11:11:57 +0700

    fix update error props

--- src/features/user/editUser.js
+++ src/features/user/editUser.js
@@ -8,11 +8,14 @@ import { editUser } from "./userSlice"
 
 
 export const EditUser = () => {
-  const users = useSelector(store => store.users);
+  const users = useSelector(store => {
+    console.log(store);
+    return store.users
+  });
   const params = useParams();
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const existingUser = users.filter(user => user.id === params.id);
+  const existingUser = users.filter(user => user.id == params.id);
   const { name, email } = existingUser[0];
   const [values, setValues] = useState({
     name,
@@ -33,14 +36,14 @@ export const EditUser = () => {
     <div className="mt-10 max-w-xl mx-auto">
       <TextField
         label="Name"
-        value={values.name}
+        values={values.name}
         onChange={(e) => setValues({ ...values, name: e.target.value })}
         inputProps={{ type: 'text', placeholder: 'Jhon Doe' }}
       />
       <br />
       <TextField
         label="Email"
-        value={values.email}
+        values={values.email}
         onChange={(e) => setValues({ ...values, email: e.target.value })}
         inputProps={{ type: 'email', placeholder: 'jhondoe@mail.com' }}
       />
--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
     <BrowserRouter>
+    <Provider store={store}>
     <App />
-    </BrowserRouter>
     </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
